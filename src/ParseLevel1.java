fd0154fd8db3b3a5bf6ec74b4f782cdf377460cf
Dio <dio@freelabs.net>
2012-04-29 20:53:23 +0300

    Updated source files with GPL licensing, added 'Licensing' folder with GPL license

--- src/ParseLevel1.java
+++ src/ParseLevel1.java
@@ -1,3 +1,27 @@
+/**
+	eradioParser: This program extracts the radio station 
+	links along with their names, found on http://e-radio.gr, 
+	so that anyone may easily create a playlist.
+    
+    Copyright (C) 2012  Lappas Dionysis
+    
+    This file is part of eradioParser.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
+    
+    You may contact the author at: dio@freelabs.net
+ */
 package noThreads;
 
 import java.io.BufferedWriter;
--- src/theMain.java
+++ src/theMain.java
@@ -1,4 +1,32 @@
+/**
+	eradioParser: This program extracts the radio station 
+	links along with their names, found on http://e-radio.gr, 
+	so that anyone may easily create a playlist.
+    
+    Copyright (C) 2012  Lappas Dionysis
+    
+    This file is part of eradioParser.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
+    
+    You may contact the author at: dio@freelabs.net
+ */
+
+
 package noThreads;
+
+
 /**
  * This program accepts command line arguments. 
  * The command line arguments supported are: 
@@ -22,6 +50,14 @@ public class theMain {
 		//how much time lasts a program execution
 		long total_time=0, startTime=0, endTime=0;
 		startTime = System.currentTimeMillis();
+		
+		/*
+		 * Print GPL message
+		 */
+		System.out.println("EradioParser Copyright (C) 2012 Lappas Dionysis \n"+
+				"This program comes with ABSOLUTELY NO WARRANTY. This is free software, and you are\n" +
+				"welcome to redistribute it under certain conditions; details: http://www.gnu.org/licenses/gpl.txt \n");
+
 		
 		/*
 		 * This structure will hold all the file names used on this project.
