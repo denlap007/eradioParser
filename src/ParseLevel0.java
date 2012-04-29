febdbc429c00e4ea3455c78663d01955aa0300e6
Dio <dio@freelabs.net>
2012-04-29 21:15:20 +0300

    Updated GPL licensing text

--- src/ParseLevel0.java
+++ src/ParseLevel0.java
@@ -3,16 +3,16 @@
 	links along with their names, found on http://e-radio.gr, 
 	so that anyone may easily create a playlist.
     
-    Copyright (C) 2012  Lappas Dionysis
+	Copyright (C) 2012  Lappas Dionysis
     
     This file is part of eradioParser.
 
-    This program is free software: you can redistribute it and/or modify
+    eradioParser is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
+    eradioParser is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
